d5f9c0ba51b5bbb96e2fa85bc18353a367f427d4
satryamuhaimin <satryamuhaiminamril@gmail.com>
2024-11-29 15:53:55 +0800

    Add files via upload

---
+++ latihan2.js
@@ -0,0 +1,8 @@
+var yakin = confirm("pilih janda atau duda");
+var nama = prompt("Siapa nama mu?", "");
+document.write("<p>hello "+ nama +"</p>");
+document.write("<p>hello "+ nama +"</p>");
+document.write("<p>hello "+ nama +"</p>");
+document.write("<p>hello "+ nama +"</p>");
+document.write("<p>hello "+ nama +"</p>");
+alert("hello janda duda")
---
+++ tugas_1.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>halaman php pertamaku</title>
+    </head>
+    <body>
+        <h1>script PHP dalam (x)html</h1>
+        <?php
+        echo "hello world!";
+        echo "hello world!";
+        echo "hello world!";
+        echo "hello world!";
+        echo "hello world!";
+        ?>
+    </body>
+ </html>
